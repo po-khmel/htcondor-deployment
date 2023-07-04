0a6a731db41fcef1ed44bdc41f64418fa35dc716
po-khmel <khmelevskayapv@gmail.com>
2023-07-04 12:41:17 +0200

    fixes

--- files/create_share.sh
+++ files/create_share.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+mkdir -p /data/share
+chown rocky:rocky -R /data/share
+
 mkfs -t xfs /dev/vdb
 echo "/dev/vdb  /data/share xfs defaults,nofail 0 2" >> /etc/fstab
 mount /data/share
-mkdir -p /data/share
-chown rocky:rocky -R /data/share
+
