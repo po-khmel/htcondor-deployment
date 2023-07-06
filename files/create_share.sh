e762813d12a6c1c29d2c5007209d3ebdf7b3b2e7
po-khmel <khmelevskayapv@gmail.com>
2023-07-06 09:53:22 +0200

    fix

--- files/create_share.sh
+++ files/create_share.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-mkdir -p /data/share
-chown rocky:rocky -R /data/share
+sudo mkdir -p /data/share
+sudo chown rocky:rocky -R /data/share
 
 mkfs -t xfs /dev/vdb
 echo "/dev/vdb  /data/share xfs defaults,nofail 0 2" >> /etc/fstab
