0b7ea6f2c89dae932ae8ef866326ea8d2d984170
po-khmel <khmelevskayapv@gmail.com>
2023-07-06 12:50:21 +0200

    update readme

--- vars.tf
+++ vars.tf
@@ -1,10 +1,46 @@
+# has to be cahged
+variable "public_key" {
+  type = map(any)
+  default = {
+    name       = "htcondor-cluster-test"
+    public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCpobhtTAVUFOgJo+WAYdGhHwmAdEL9JuE15iUZ2d098yZnhAJ2NkfeCPWtL3dsUYEAAtPqH2QKlgHtUR6aPQRabMPCcntwa+x1S2wXN457xtnnKZo/N8yC1bjhry6wzqqNwbyo4lKqJgsS3AIMQuPDBFvidQg4PleQfisv1uZvL9+9/mm/DIJ5Sa5t5JR2RIajY1agAvgsS3yZzgCFAVi45UlAICAFnYQwCbpF2Y9rXSG9MZtUecg3m57CU3zxCH7+1zdt6NBEINlZYR3v0NQ60o4OFbHKvHDBdKfutz/rT6H5EjULsWScwkpi+L4V5SmdBUk1JobztacHiYgTj/pJ centos@pulsar-control-vm.garr.cloud.pa"
+  }
+}
+
+variable "public_network" {
+  type = map(any)
+  default = {
+    name = "floating-ip"
+  }
+}
+
+variable "private_network" {
+  type = map(any)
+  default = {
+    name        = "elixir-VM-net"
+    subnet_name = "elixir-VM-subnet"
+    cidr4       = "192.168.208.0/22"
+  }
+}
+
+# uncomment `sensitive = true` if you don't want to expose these values in terraform state file
+# this var should be specified in terraform.tfvars
+variable "pvt_key" {
+  # sensitive = true
+}
+
+# this var should be specified in terraform.tfvars
+variable "condor_pass" {
+  # sensitive = true
+}
+
 variable "nfs" {
   type = map(any)
   default = {
-    "name"        = "nfs-htcondor"
-    "description" = "nfs volume for htcondor cluster"
-    "disk_size"   = 300
-    "volume_type" = "__DEFAULT__"
+    name        = "nfs-htcondor"
+    description = "nfs volume for htcondor cluster"
+    disk_size   = 300
+    volume_type = "__DEFAULT__"
   }
 }
 
@@ -19,25 +55,17 @@ variable "flavors" {
 
 variable "exec_node_count" {
   type    = number
-  default = 1
+  default = 2
 }
 
 variable "image" {
   type = map(any)
   default = {
-    "name"             = "Rocky-9-GenericCloud.latest.x86_64"
-    "image_source_url" = "https://download.rockylinux.org/pub/rocky/9/images/x86_64/Rocky-9-GenericCloud.latest.x86_64.qcow2"
-    "container_format" = "bare"
-    "disk_format"      = "qcow2"
-    "description"      = "downloaded from https://download.rockylinux.org"
-  }
-}
-
-variable "public_key" {
-  type = map(any)
-  default = {
-    name       = "htcondor-cluster-test"
-    public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCpobhtTAVUFOgJo+WAYdGhHwmAdEL9JuE15iUZ2d098yZnhAJ2NkfeCPWtL3dsUYEAAtPqH2QKlgHtUR6aPQRabMPCcntwa+x1S2wXN457xtnnKZo/N8yC1bjhry6wzqqNwbyo4lKqJgsS3AIMQuPDBFvidQg4PleQfisv1uZvL9+9/mm/DIJ5Sa5t5JR2RIajY1agAvgsS3yZzgCFAVi45UlAICAFnYQwCbpF2Y9rXSG9MZtUecg3m57CU3zxCH7+1zdt6NBEINlZYR3v0NQ60o4OFbHKvHDBdKfutz/rT6H5EjULsWScwkpi+L4V5SmdBUk1JobztacHiYgTj/pJ centos@pulsar-control-vm.garr.cloud.pa"
+    name             = "Rocky-9-GenericCloud.latest.x86_64"
+    image_source_url = "https://download.rockylinux.org/pub/rocky/9/images/x86_64/Rocky-9-GenericCloud.latest.x86_64.qcow2"
+    container_format = "bare"
+    disk_format      = "qcow2"
+    description      = "downloaded from https://download.rockylinux.org"
   }
 }
 
@@ -63,22 +91,6 @@ variable "secgroups" {
   ]
 }
 
-variable "public_network" {
-  type = map(any)
-  default = {
-    "name" = "floating-ip"
-  }
-}
-
-variable "private_network" {
-  type = map(any)
-  default = {
-    name        = "elixir-VM-net"
-    subnet_name = "elixir-VM-subnet"
-    cidr4       = "192.168.208.0/22"
-  }
-}
-
 variable "domain_name" {
   type    = string
   default = "htcondor"
@@ -89,11 +101,4 @@ variable "ssh-port" {
   default = "22"
 }
 
-variable "pvt_key" {
-  # sensitive = true
-}
-
-variable "condor_pass" {
-  # sensitive = true
-}
 
